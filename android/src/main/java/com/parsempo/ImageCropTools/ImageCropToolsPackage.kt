02e1d0a249a0d0bd7dfac9da0f31a68a5cf751c9
e.uvarov <Eugene.Uvarov@finstek.com>
2021-10-17 14:54:21 +0300

    Fixed

--- android/src/main/java/com/parsempo/ImageCropTools/ImageCropToolsPackage.kt
+++ android/src/main/java/com/parsempo/ImageCropTools/ImageCropToolsPackage.kt
@@ -1,13 +1,18 @@
 package com.parsempo.ImageCropTools
 
 import com.facebook.react.ReactPackage
+import com.facebook.react.bridge.JavaScriptModule
 import com.facebook.react.bridge.NativeModule
 import com.facebook.react.bridge.ReactApplicationContext
 import com.facebook.react.uimanager.ViewManager
 
-abstract class ImageCropToolsPackage : ReactPackage {
+class ImageCropToolsPackage : ReactPackage {
     override fun createNativeModules(reactContext: ReactApplicationContext): List<NativeModule> {
         return mutableListOf(ImageCropToolsModule(reactContext))
+    }
+
+    override fun createJSModules(): MutableList<Class<out JavaScriptModule>> {
+        return mutableListOf()
     }
 
     override fun createViewManagers(reactContext: ReactApplicationContext): List<ViewManager<*, *>> {
